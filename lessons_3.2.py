cff8d09b8ced9f502cc2e9bca17728e3fb78f929
Beslan <bes-zaurbaev@yandex.ru>
2022-11-22 08:55:33 +0300

    Чтение с сокета клиента

---
+++ lessons_3.2.py
@@ -0,0 +1,24 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ('127.0.0.1', 8000)
+server_socket.bind(server_address)
+server_socket.listen()
+
+try:
+    conection, client_address = server_socket.accept()
+    print(f'Подключен клиент с адресом: {client_address}')
+
+    buffer = b''
+
+    while buffer[-2:] != b'\r\n':
+        data = server_socket.recv(2)
+        if not data:
+            break
+        else:
+            buffer += data
+        print('Все полученные данные', buffer)
+finally:
+    server_socket.close()
